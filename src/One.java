9a0744bb1ed09dbddad2be63bbc79de182185329
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-10-18 19:48:51 +0300

    isAlive And Join

--- src/One.java
+++ src/One.java
@@ -1,16 +1,17 @@
 class Second  implements Runnable{
     private String name;
+    Thread t ;
     Second(String threadname){
         name = threadname;
-        Thread t = new Thread(this,"Demonstration one");
-        System.out.println("First thread created: " + t);
+        t = new Thread(this,name);
+        System.out.println("New thread created: " + t);
         t.start();
     }
     public void run(){
         try{
             for(int i = 5;i > 0; i--){
                 System.out.println(name +": " + i);
-                Thread.sleep(500);
+                Thread.sleep(1000);
             }
         }catch(InterruptedException e){
             System.out.println(name + " thread interrupted");
@@ -21,16 +22,26 @@ class Second  implements Runnable{
 
 public class One {
     public static void main(String...args){
-        new Second("one");
-        new Second("two");
-        new Second("three");
+        Second ob1 = new Second("one");
+        Second ob2 = new Second("two");
+        Second ob3 = new Second("three");
+
+        System.out.println("Thread one alive: " + ob1.t.isAlive());
+        System.out.println("Thread one alive: " + ob2.t.isAlive());
+        System.out.println("Thread one alive: " + ob3.t.isAlive());
 
         try{
-            Thread.sleep(10000);
+            System.out.println("waiting for threads off");
+            ob1.t.join();
+            ob2.t.join();
+            ob3.t.join();
 
         }catch(InterruptedException e){
             System.out.println("Main thread interrapted");
         }
+        System.out.println("Thread one alive: " + ob1.t.isAlive());
+        System.out.println("Thread one alive: " + ob2.t.isAlive());
+        System.out.println("Thread one alive: " + ob3.t.isAlive());
 
         System.out.println("Main thread off");
     }
