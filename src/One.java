9beec0785c6d61fdbb5513b94b5db9c270e133c8
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2021-10-18 18:50:47 +0300

    MultiThread

--- src/One.java
+++ src/One.java
@@ -1,31 +1,33 @@
-class Second  extends Thread{
-    Second(){
-        super("Demonstration one");
-        System.out.println("First thread created: " + this);
-        start();
+class Second  implements Runnable{
+    private String name;
+    Second(String threadname){
+        name = threadname;
+        Thread t = new Thread(this,"Demonstration one");
+        System.out.println("First thread created: " + t);
+        t.start();
     }
     public void run(){
         try{
             for(int i = 5;i > 0; i--){
-                System.out.println("Demon thread: " + i);
+                System.out.println(name +": " + i);
                 Thread.sleep(500);
             }
         }catch(InterruptedException e){
-            System.out.println("First thread interrapted");
+            System.out.println(name + " thread interrupted");
         }
-        System.out.println("First thread off");
+        System.out.println(name + " thread off");
     }
 }
 
 public class One {
     public static void main(String...args){
-        new Second();
+        new Second("one");
+        new Second("two");
+        new Second("three");
 
         try{
-            for(int i = 5 ; i > 0; i--){
-                System.out.println("Main Thread: " + i);
-                Thread.sleep(1000);
-            }
+            Thread.sleep(10000);
+
         }catch(InterruptedException e){
             System.out.println("Main thread interrapted");
         }
